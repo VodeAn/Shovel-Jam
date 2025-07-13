40915cc476cbdcd792d11f45bcc299173da0a3ea
VodeAn <123888284+VodeAn@users.noreply.github.com>
2025-07-13 21:07:09 +0300

    created a method

--- Jam 1/Assets/Scripts/GameManager.cs
+++ Jam 1/Assets/Scripts/GameManager.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   
+    
+    public void Anxiety(float anxietyMeter)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (anxietyMeter < 100)
+        {
+            anxietyMeter += Time.deltaTime;
+        }
+        else
+        {
+            anxietyMeter = 100;
+        }
     }
 }
